b54d10c0ee372719606099307e7325cc34993209
ghkd1418 <1004wipi@gmail.com>
2023-08-03 22:52:17 +0900

    feat: meta tag, favicon 추가

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,13 +1,30 @@
-import { Html, Head, Main, NextScript } from 'next/document'
+import { Html, Head, Main, NextScript } from "next/document";
 
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        <meta property="og:type" content="website" />
+        <meta property="og:url" content="https://hairthon.vercel.app" />
+        <meta property="og:title" content="SELECT MY HAIR" />
+        <meta
+          property="og:image"
+          content="https://hairthon.vercel.app/public/og.svg"
+        />
+        <meta
+          property="og:description"
+          content="이젠 더이상 머리 스타일로 고민하지 마세요! 나에게 어울리는 머리 스타일을 합성해보세요!"
+        />
+        <meta property="og:site_name" content="SELECT MY HAIR" />
+        <meta property="og:locale" content="ko_KO" />
+        <meta property="og:image:width" content="1200" />
+        <meta property="og:image:height" content="630" />
+        <link rel="icon" href="/favicon.svg" />
+      </Head>
       <body>
         <Main />
         <NextScript />
       </body>
     </Html>
-  )
+  );
 }
