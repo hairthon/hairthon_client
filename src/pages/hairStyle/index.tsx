c66127864b59e3a792c1ecfe1092a6fb2c5465f1
ghkd1418 <1004wipi@gmail.com>
2023-08-05 02:36:43 +0900

    feat: img

--- src/pages/hairStyle/index.tsx
+++ src/pages/hairStyle/index.tsx
@@ -6,17 +6,14 @@ export default function HairStyle() {
 
   useEffect(() => {
     const img = localStorage.getItem("image") ?? "";
-    console.log(img);
-
     const blob = new Blob([img], { type: "image/jpeg" });
     const imageUrl = URL.createObjectURL(blob);
-    console.log(imageUrl);
-    const im = imageUrl.replace("blob:", "");
-    setImage(im);
+    console.log("🚀 ~ file: index.tsx:11 ~ useEffect ~ imageUrl:", imageUrl);
+    setImage(imageUrl);
   }, []);
 
   return (
-    <main className={`flex flex-col items-center h-full bg-white py-10`}>
+    <main className={`flex flex-col items-center h-screen bg-white py-10`}>
       <Image
         src={"/subtract.svg"}
         alt="landing"
@@ -33,7 +30,7 @@ export default function HairStyle() {
           height={300}
           priority
         /> */}
-        <img src={image} alt="" />
+        {image && <img src={image} alt="" />}
         <div className="bg-gray-400 w-[200px] h-[300px] mt-20" />
         <div className="absolute top-20 drop-shadow-lg bg-gradient-to-b from-green-300 to-green-500 w-full h-1.5 from-transparent to-opacity-80 animate-[scanning_1.5s_linear_infinite]" />
       </div>
