856f4032ed6266296947e8294ff4388bfac596c2
ghkd1418 <1004wipi@gmail.com>
2023-08-05 18:56:06 +0900

    chore: className 띄어쓰기

--- src/components/UploadImage.tsx
+++ src/components/UploadImage.tsx
@@ -270,7 +270,7 @@ export function UploadImage() {
             )
           ) : !isLoading ? (
             <button
-              className={`px-3 py-2 rounded-lg mt-5 text-white${
+              className={`px-3 py-2 rounded-lg mt-5 text-white ${
                 selectedImage2 ? "bg-black hover:bg-sky-600" : "bg-gray-400"
               }`}
               onClick={handleSynthesis}
